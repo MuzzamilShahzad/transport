006a8d57bfecdb2a0392125c7eb641346cd1130d
ahsan <ahsanishaq321@gmail.com>
2022-06-02 15:14:22 +0500

    Update Contractor Add File

--- resources/views/contractor/add.blade.php
+++ resources/views/contractor/add.blade.php
@@ -4,7 +4,6 @@
 
 <div class="main-content side-content pt-0">
     <div class="main-container container-fluid">
-        .................
         <div class="inner-body">
             <!-- Page Header -->
             <div class="page-header">
